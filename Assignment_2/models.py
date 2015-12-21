1e755c5d595169143e021068a63f2573a3b50f6b
Fissal <fissalalsharef@gmail.com>
2015-12-22 01:59:55 +0200

    Initial commit

---
+++ Assignment_2/models.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Teacher(models.Model):
+    Teacher_Name = models.CharField(max_length = 70)
+    Teacher_LastName = models.CharField(max_length = 70)
+    Teacher_Email = models.EmailField()
+    Teacher_Phone = models.IntegerField()
+    Teacher_Office = models.CharField(max_length = 500)
+
+class Courses(models.Model):
+    C_Name= models.CharField(max_length = 70)
+    C_Code = models.CharField(max_length = 30)
+    C_Classroom = models.CharField(max_length = 30)
+    C_Time = models.DateTimeField(auto_now_add = True, auto_now = False)
+    C_Teacher = models.ForeignKey(Teacher)
+    C_Students = models.ManyToManyField('Student')
+
+class Student(models.Model):
+    Student_Name = models.CharField(max_length = 70)
+    Student_LastName = models.CharField(max_length = 70)
+    Student_Courses = models.ManyToManyField('Courses')
+    Student_Email = models.EmailField()
+
+
